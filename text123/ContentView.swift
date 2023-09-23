756cee01a72a859753f594114f7e3f7907965bd9
thanachok <ext.thanachok.rat@truedigital.com>
2023-09-23 20:47:39 +0700

    add 123

---
+++ text123/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  text123
+//
+//  Created by Schweppe on 23/9/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!1bjj")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ text123/text123App.swift
@@ -0,0 +1,17 @@
+//
+//  text123App.swift
+//  text123
+//
+//  Created by Schweppe on 23/9/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct text123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
